d7f8bc7dda0cf48252952aeb0ec2455e5e50a0c4
Echo Shih <chunyushih@outlook.com>
2022-05-31 18:54:30 +0800

    [feat] - adjested finished time interval display due to grammer constraits

--- src/pages/ToDoList/components/TimeFilter.js
+++ src/pages/ToDoList/components/TimeFilter.js
@@ -1,22 +1,75 @@
 import { useRef } from "react";
+import { useTranslation } from "react-i18next";
 const moment = require("moment");
 moment.locale("zh-tw");
 
 const TimeFilter = ({ finishedTime }) => {
+  const currentLng = localStorage.getItem("i18nextLng");
+  const { t } = useTranslation(["task"]);
   const timeDisplay = useRef("");
-  const timeAgo = Math.floor((Date.now() - finishedTime) / 1000);
-  const lessMinute = timeAgo < 60;
-  const lessHours = timeAgo < 60 * 60;
-  const lessDay = timeAgo < 60 * 60 * 24;
 
-  if (lessMinute) {
-    timeDisplay.current = `${timeAgo + " 秒前"}`;
-  } else if (lessHours) {
-    timeDisplay.current = `${Math.floor(timeAgo / 60) + " 分鐘前"}`;
-  } else if (lessDay) {
-    timeDisplay.current = `${Math.floor(timeAgo / (60 * 60)) + " 小時前"}`;
+  const SECONDS_PER_MINUTE = 60;
+  const SECONDS_PER_TWO_MINUTES = 120;
+  const SECONDS_PER_HOUR = 3_600;
+  const SECONDS_PER_TWO_HOURS = 7_200;
+  const SECONDS_PER_DAY = 86_400;
+  const LANGUAGES_WITH_DIFFERENT_GRAMMAR = ["de-DE", "fr-FR", "es-ES"];
+
+  const timeAgo = Math.floor((Date.now() - finishedTime) / 1000);
+  const lessThanAMinute = timeAgo < SECONDS_PER_MINUTE;
+  const lessThanAnHour = timeAgo < SECONDS_PER_HOUR;
+  const lessThanADay = timeAgo < SECONDS_PER_DAY;
+
+  if (
+    LANGUAGES_WITH_DIFFERENT_GRAMMAR.some((language) => language === currentLng)
+  ) {
+    if (lessThanAMinute) {
+      timeAgo === 1
+        ? (timeDisplay.current = `${t("ago")} ${timeAgo} ${t("second")}`)
+        : (timeDisplay.current = `${t("ago")} ${timeAgo} ${t("seconds")}`);
+    } else if (lessThanAnHour) {
+      timeAgo >= SECONDS_PER_MINUTE && timeAgo < SECONDS_PER_TWO_MINUTES
+        ? (timeDisplay.current = `${t("ago")} ${Math.floor(timeAgo / 60)}  ${t(
+            "minute"
+          )}`)
+        : (timeDisplay.current = `${t("ago")} ${Math.floor(timeAgo / 60)}  ${t(
+            "minutes"
+          )}`);
+    } else if (lessThanADay) {
+      timeAgo >= SECONDS_PER_HOUR && timeAgo < SECONDS_PER_TWO_HOURS
+        ? (timeDisplay.current = `${t("ago")} ${Math.floor(
+            timeAgo / (60 * 60)
+          )} ${t("hour")}`)
+        : (timeDisplay.current = `${t("ago")} ${Math.floor(
+            timeAgo / (60 * 60)
+          )} ${t("hours")}`);
+    } else {
+      timeDisplay.current = `${moment(finishedTime).format("YYYY-MM-DD")}`;
+    }
   } else {
-    timeDisplay.current = `${moment(finishedTime).format("YYYY-MM-DD")}`;
+    if (lessThanAMinute) {
+      timeAgo === 1
+        ? (timeDisplay.current = `${timeAgo} ${t("secondAgo")}`)
+        : (timeDisplay.current = `${timeAgo} ${t("secondsAgo")}`);
+    } else if (lessThanAnHour) {
+      timeAgo >= SECONDS_PER_MINUTE && timeAgo < SECONDS_PER_TWO_MINUTES
+        ? (timeDisplay.current = `${Math.floor(timeAgo / 60)}  ${t(
+            "minuteAgo"
+          )}`)
+        : (timeDisplay.current = `${Math.floor(timeAgo / 60)}  ${t(
+            "minutesAgo"
+          )}`);
+    } else if (lessThanADay) {
+      timeAgo >= SECONDS_PER_HOUR && timeAgo < SECONDS_PER_TWO_HOURS
+        ? (timeDisplay.current = `${Math.floor(timeAgo / (60 * 60))} ${t(
+            "hourAgo"
+          )}`)
+        : (timeDisplay.current = `${Math.floor(timeAgo / (60 * 60))} ${t(
+            "hoursAgo"
+          )}`);
+    } else {
+      timeDisplay.current = `${moment(finishedTime).format("YYYY-MM-DD")}`;
+    }
   }
 
   return <div>{timeDisplay.current}</div>;
